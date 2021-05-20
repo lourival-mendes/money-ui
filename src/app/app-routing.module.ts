841de1deca788e4f9372ea5371c936c6001e688c
lourival-mendes <minhavirtude@gmail.com>
2021-05-20 19:13:00 -0300

    Alterações para carregamento tardio de scripts.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.compo
 
 const routes: Routes = [
 
+  { path: 'lancamentos', loadChildren: () => import('./lancamentos/lancamentos.module').then(response => response.LancamentosModule) },
+  { path: 'pessoas', loadChildren: () => import('./pessoas/pessoas.module').then(response => response.PessoasModule) },
+  { path: 'login', loadChildren: () => import('./seguranca/seguranca.module').then(response => response.SegurancaModule) },
+
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'nao-autorizado', component: NaoAutorizadoComponent },
   { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
+import { TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
+
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SegurancaModule } from './seguranca/seguranca.module';
-import { LancamentosModule } from './lancamentos/lancamentos.module';
-import { PessoasModule } from './pessoas/pessoas.module';
 
 @NgModule({
   declarations: [
@@ -22,8 +21,6 @@ import { PessoasModule } from './pessoas/pessoas.module';
     TranslateModule.forRoot(),
 
     CoreModule,
-    LancamentosModule,
-    PessoasModule,
     SegurancaModule,
     AppRoutingModule
   ],
